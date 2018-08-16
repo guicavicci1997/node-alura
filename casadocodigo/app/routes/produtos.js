c9bf81302adc9f1ab6158b25341c24676cffbe5f
gneves <gneves@mmcafe.com.br>
2018-08-16 10:34:58 -0300

    cadastrando produto no banco

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,18 +1,46 @@
 module.exports = function(app){
     app.get('/produtos', function(req,res){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
         
-        produtosBanco.lista(function(errors, results){
-            res.send(results);
-            //res.render('produtos/lista', {lista:results})
-
+        produtosDAO.lista(function(erros,resultados){
+            res.format({
+                html: function(){
+                    res.render('produtos/lista',{lista:resultados});
+                },
+                json: function(){
+                    res.json(resultados)
+                }
+            });
+    
         });
-      
         connection.end();
 
+    });
+
+        app.get('/produtos/form',function(req,res){
+            res.render('produtos/form');
         });
 
-        app.get ('produtos/remove', function(){
-    });
+        //Post salvando o form
+        app.post('/produtos', function(req,res){
+
+            var produto = req.body;
+
+            // var validatorPreco = req.assert ('preco','O preco é obrigatorio')
+            // validatorPreco.notEmpty();
+
+            // var errors = req.validationErrors();
+            // if(errors){
+            //     res.render('produtos/form')
+            // }
+            
+            var connection = app.infra.connectionFactory();
+            var produtosDAO = new app.infra.ProdutosDAO(connection);
+            produtosDAO.salva(produto,function(errors, resultados){
+                res.redirect('/produtos');
+
+            });
+
+        });
 }
